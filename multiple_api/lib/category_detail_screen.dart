80d1eb8467baf1aa7828fa7516ed0a446fe710aa
KshitijH123 <kshitijhapase@gmail.com>
2024-09-17 19:47:43 +0530

    card design

--- multiple_api/lib/category_detail_screen.dart
+++ multiple_api/lib/category_detail_screen.dart
@@ -1,4 +1,3 @@
-// screens/category_detail_screen.dart
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:multiple_api/model_class.dart/get_api_categories_controller.dart';
@@ -23,18 +22,30 @@ class CategoryDetailScreen extends StatelessWidget {
                   itemCount: controller.categories.length,
                   itemBuilder: (context, index) {
                     CategoryDetailModel category = controller.categories[index];
-                    return ListTile(
-                      title: Text(category.name),
-                      subtitle: Text(category.slug),
-                      onTap: () {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('URL: ${category.url}'),
-                            backgroundColor: Colors.green,
-                            duration: const Duration(seconds: 2),
-                          ),
-                        );
-                      },
+                    return Card(
+                      elevation: 4.0,
+                      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.all(16.0),
+                        title: Text(
+                          category.name,
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        subtitle: Text(
+                          category.slug,
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                        onTap: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('URL: ${category.url}'),
+                              backgroundColor: Color.fromARGB(255, 46, 208, 24),
+                              duration: const Duration(seconds: 3),
+                            ),
+                          );
+                        },
+                      ),
                     );
                   },
                 );
